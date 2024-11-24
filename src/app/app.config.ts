3c40289a667683a0b861469f81fba69fa4d5b14e
Rupali <sharmarupali2603@gmail.com>
2024-11-24 22:34:15 +0530

    New code

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { WebcamModule } from 'ngx-webcam';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
