e16c923b4213c0c4170a8736cadf80e313a41a7b
Rupali <sharmarupali2603@gmail.com>
2024-11-25 10:07:04 +0530

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,54 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { WebcamImage, WebcamModule } from 'ngx-webcam';
+import { every, Observable, Subject } from 'rxjs';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule,RouterOutlet,WebcamModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'AngularAssign';
+stream:any=null;
+status:any=null;
+trigger:Subject<void>=new Subject();
+previewImage:string='';
+buttonLabel:string='Capture Image';
+get $trigger(): Observable<void>{
+  return this.trigger.asObservable();
+}
+
+snapshot(event:WebcamImage){
+  console.log(event);
+this.previewImage=event.imageAsDataUrl;
+this.buttonLabel="Re Capture Image"
+}
+  checkPermission(){
+    navigator.mediaDevices.getUserMedia({
+     video:{
+      width:500,
+      height:400
+    }}
+).then((res) =>{
+console.log("response,",res);
+this.stream=res;
+this.status="Camera is accessing"
+this.buttonLabel="Capture Image"
+    }).catch(err=>{
+      console.log("error,",err);
+      this.status=err;
+      if(err?.message=="Permission denied"){
+        this.status="Permission denied please try again by approving the camera access."
+      }else{
+        this.status="You may not having camera in your device. Please try again...."
+      }
+    })
+  }
+
+  captureImage(){
+this.trigger.next();
+
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { WebcamModule } from 'ngx-webcam';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    WebcamModule
   ]
 })
 export class AppModule { }
